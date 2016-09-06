36f8a7d0cfb6f3bfdc6ff0a3aa73d4ccd3a6b3bb
Paolo <guzzopaul@gmail.com>
2016-09-06 16:50:09 +0200

    ciaone

--- students/2016-09-04/Paolo/hello.py
+++ students/2016-09-04/Paolo/hello.py
@@ -1,4 +1,9 @@
 # -*- coding: utf-8 -*-
+"""
+Questa à una doc string
+su più linee
+"""
+
 
 def do_hello(input_saluto): #Una funzione è un simbolo all'interno di un modulo (noi possiamo importare anche solo alcuni simboli di un modulo che sono la nostra libreria)
     print(u"Sono dentro la funzione e mi è arrivato questo input: " + input_saluto)
@@ -6,7 +11,14 @@ def do_hello(input_saluto): #Una funzione è un simbolo all'interno di un modulo
     print("tot vale " + str(tot))
 	
 def do_hello_init_param(input_saluto = "pippo"):
-    print("Ecco {}".format(input_saluto))
+
+    try:
+        message = "Ecco " + input_saluto
+        print("Result Message: {}".format(message))
+    except TypeError:
+        print("Errore di tipo")
+    finally:
+        print("Siamo alla fine")
 
 # La funzione finisce quando finsice l'indentazione
 # Se vieni importato come libreria e non per essere eseguito non printare.
