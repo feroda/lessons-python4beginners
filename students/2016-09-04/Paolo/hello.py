e95aebdd84e246f7cf2658e263de56dddc26b642
Paolo <guzzopaul@gmail.com>
2016-09-06 15:25:05 +0200

    ggiunto il parametro e un po di cavoli miei

--- students/2016-09-04/Paolo/hello.py
+++ students/2016-09-04/Paolo/hello.py
@@ -4,8 +4,13 @@ def do_hello(input_saluto): #Una funzione è un simbolo all'interno di un modulo
     print(u"Sono dentro la funzione e mi è arrivato questo input: " + input_saluto)
     tot = 1 + 1
     print("tot vale " + str(tot))
+	
+def do_hello_init_param(input_saluto = "pippo"):
+    print("Ecco {}".format(input_saluto))
 
 # La funzione finisce quando finsice l'indentazione
 # Se vieni importato come libreria e non per essere eseguito non printare.
 if __name__ == "__main__":
     do_hello("Ciaone")
+    do_hello_init_param() 
+	#Posso chiamarla senza mandare nulla perché questa funzione ha il parametro già inizializzato.
