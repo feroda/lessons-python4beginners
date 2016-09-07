fbeed46dd82d0a82aa54bad6f87d1d3001627719
Elena De Angelis <e.deangelis@teamsystem.com>
2016-09-07 09:32:02 +0200

    [PYTEST] aggiunti file di test hello e fibonacci

--- src/hello.py
+++ src/hello.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 
-def do_hello(who):
-    #il carattere "u" prima di una stringa definisce una stringa unicode
-    print (u"ciao {}" .format(who))  # equivale a print (u"Ciao %s" % who)
+def compose_hello(who, force=False):
+    try:
+	    message = "Hello " + who + "!"
+    except TypeError:
+	    print ("[WARNING]: il parametro 'who' dovrebbe essere una stringa")
 			
 if __name__ == "__main__":
     #questa stringa viene stampata solo se viene eseguito e non se viene
     #importato:
     #doppio underscore -> metodo privato
-    do_hello(u"Elena")
+    do_hello()
