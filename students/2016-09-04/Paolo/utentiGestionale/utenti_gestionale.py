6fb997ac54e25539da6c07a6edd76fa25d1f87d5
Paolo <guzzopaul@gmail.com>
2016-09-07 17:53:03 +0200

    Gestionale utenti

--- students/2016-09-04/Paolo/utentiGestionale/utenti_gestionale.py
+++ students/2016-09-04/Paolo/utentiGestionale/utenti_gestionale.py
@@ -1,6 +1,41 @@
 # -*- coding: utf-8 -*-
 
 import csv  
+import json
+import xml.etree.cElementTree as ET
+
+def print_on_screen(list_input):
+    for i,p in enumerate(list_input):
+        print("Utente {}".format(i))
+        
+        print("     Name: {}".format(p["name"]))
+        print("     City: {}".format(p["city"]))
+        print("     Salary: {}".format(p["salary"]))
+        
+        
+def write_on_csv(list_input):
+    with open("people.csv", "wb") as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        spamwriter.writerow(list_input)
+        
+
+def write_on_json(list_input):
+    with open("people.json", "wb") as jsonfile:
+        json.dump(list_input, jsonfile)
+        
+  
+def write_on_xml(list_input):
+    root = ET.Element("root")
+    doc = ET.SubElement(root, "doc")
+    utenti_child = ET.SubElement(doc, "Utenti")
+    
+    for i,p in enumerate(list_input):
+        ET.SubElement(utenti_child, "utente " + str(i), name="name").text = p["name"]
+        ET.SubElement(utenti_child, "utente " + str(i), name="city").text = p["city"]
+        ET.SubElement(utenti_child, "utente " + str(i), name="salary").text = str(p["salary"])
+        
+    tree = ET.ElementTree(root)
+    tree.write("people.xml")
 
 def main():
     
@@ -16,11 +51,11 @@ def main():
         anagraphic_people_dict["salary"] = int(salary)
         PEOPLE.append(anagraphic_people_dict)
         
-    print(PEOPLE)
+    print_on_screen(PEOPLE)      
+    write_on_csv(PEOPLE)
+    write_on_json(PEOPLE)
+    write_on_xml(PEOPLE)
     
-    with open("people.csv", "wb") as csvfile:
-        spamwriter = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        spamwriter.writerow(PEOPLE)
 
 
 if __name__ == "__main__":
