44fcd2c71cc676e6def82951ec66373a9baedf26
Elena De Angelis <e.deangelis@teamsystem.com>
2016-09-08 12:39:04 +0200

    [GESTIONALE] programma gestionale e fiobancci

--- src/fibonacci.py
+++ src/fibonacci.py
@@ -3,14 +3,13 @@
 def succ_fib_from_string(x):
     s = "0 1 1 2 3 5 8"
     if x < 0:
-        raise ValueError
+        raise ValueError("Indice negativo non funziona")
     else:
         try:
-           i = x * 2
-        except TypeError:
-           raise
+           return int(s[x*2])
+        except IndexError:
+           raise NotImplementedError("non implementato")
         
-        return int(s[i])
  
 def succ_fib_from_list(x):
     l = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597]
@@ -18,11 +17,10 @@ def succ_fib_from_list(x):
         raise ValueError
     else:
         try:
-           l[x]
-        except TypeError:
-           raise
+            return l[x]
+        except IndexError:
+           raise NotImplementedError("non implementato")
         
-        return l[x]
    
 def succ_fib_algo(x):
     if x < 0:
@@ -30,14 +28,23 @@ def succ_fib_algo(x):
     elif x <= 1:
         ris = x
     else:
+        ris = succ_fib_algo(x - 1) + succ_fib_algo(x - 2)
         try:
-            ris = succ_fib_algo(x - 1) + succ_fib_algo(x - 2)
-        except TypeError:
-           raise
+            return ris
+        except IndexError:
+            raise NotImplementedError("non implementato")
+            
+               
+def succ_fib_algo_luca_bolli(x):
+    fib_serie = [0,1]
+    
+    for f in range (1, x):
+        fib_serie.append(fib_serie[-1] + fib_serie[-1])
         
-    return ris
+    return int(fib_serie[i])
+        
 
-succ_fib = succ_fib_algo
+succ_fib = succ_fib_from_string
 
 if __name__ == "__main__":
     ris = succ_fib(3)
