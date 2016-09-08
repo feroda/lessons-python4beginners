02f77ba8399a5b0c7477d2dc351949d4ca953b13
Luca Ferroni <luca@befair.it>
2016-09-08 16:52:54 +0200

    [OOP] Exporters multiplo basilare

---
+++ src/gestionale/exporters01.py
@@ -0,0 +1,41 @@
+import json
+import csv
+
+
+people_fname = r"c:\Users\gigi\lessons-python4beginners\src\gestionale\people_fixture.json"
+with open(people_fname, "rb") as fpeople:
+    PEOPLE = json.load(fpeople)
+outfname = r"c:\Users\gigi\a.txt"
+
+
+class DebugExporter(object):
+    def do_export(self, f, rows):
+        for row in rows:
+            print("{}\n".format(row))
+            
+class Exporter(object):
+    def do_export(self, f, rows):
+        for row in rows:
+            f.write("{}\n".format(row))
+
+class JsonExporter(object):
+    def do_export(self, f, rows):
+        json.dump(rows, f, indent=2)
+
+
+class CsvExporter(object):
+    def do_export(self, f, rows):
+        
+        fieldnames = rows[0].keys()
+        writer = csv.DictWriter(
+            f, fieldnames = fieldnames, delimiter = ";")
+        writer.writeheader()
+        
+        for row in rows:
+            writer.writerow(row)
+
+
+def apply_exportation(xp, fname, data):
+
+    with open(fname, "wb") as f:
+        xp.do_export(f, rows=data)
