ab0aee3209702c0098f5ea2e7716d761264b91d1
Luca Ferroni <luca@befair.it>
2016-09-08 14:09:57 +0200

    [HELLO] unicode file di prova

---
+++ src/hello/hello_unicode.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+def do_hello(who=u"papà"):
+	"""
+	Funzione che saluta.
+	
+	:param str who: a chi saluto.
+	"""
+	print(u"Ciao {}".format(who))  # meglio format
+	print(u"Ciao %s" % who)
+	
+#print("__name__ = {}".format(__name__))
+if __name__ == "__main__":
+	do_hello()
