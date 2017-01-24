16b7d3c36acb9e37d4f67ee2a986950025fcc6b4
Luca Ferroni <luca@befair.it>
2017-01-24 09:07:54 +0100

    test_export_repr_all ok

--- test_gestifibo.py
+++ test_gestifibo.py
@@ -1,19 +1,20 @@
 # -*- coding: utf-8 -*-
 import unittest
 import codecs
+import tempfile
 
 import gestifibo
 
 class TestGestiFibo(unittest.TestCase):
-    
+       
     def setUp(self):
         self.p = [
             {"name": "Gianni", "city": "Napoli", "salary": 3000, "genfibo": 5},
-            {"name": "Simone", "city": "Pesaro", "salary": 3300, "genfibo": 7},
-            {"name": "Gabriele", "city": "Faenza", "salary": 2900, "genfibo": 12},
+            # {"name": "Simone", "city": "Pesaro", "salary": 3300, "genfibo": 7},
+            # {"name": "Gabriele", "city": "Faenza", "salary": 2900, "genfibo": 12},
             {"name": u"Fabiòòòò", "city": "Ascoli", "salary": 300, "genfibo": 8},
-            {"name": "Andrea", "city": "Ancona", "salary": 200, "genfibo": 32},
-            {"name": "Davide", "city": "Rimini", "salary": 2300, "genfibo": 1},
+            # {"name": "Andrea", "city": "Ancona", "salary": 200, "genfibo": 32},
+            # {"name": "Davide", "city": "Rimini", "salary": 2300, "genfibo": 1},
         ]
 
     # def tearDown(self):
@@ -21,10 +22,14 @@ class TestGestiFibo(unittest.TestCase):
 
     def test_export_repr_all(self):
         # content = u""
-        gestifibo.export_repr_all(self.p)
+
+        f, fname = tempfile.mkstemp()
+        gestifibo.export_repr_all(self.p, fname=fname)
+        with codecs.open(fname, "rb") as f:
+            exported_content = f.read()
         with codecs.open("fixtures/test_export_all.txt", "rb") as f:
             content = f.read()
-        self.assertEqual(gestifibo.export_repr_all(self.p), content)
+        self.assertEqual(exported_content, content)
 
     def test_export_repr_line_by_line(self):
         # content = u""
