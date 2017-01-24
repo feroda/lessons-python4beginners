ec307d750df590be714d970dc9498671ac31b117
Luca Ferroni <luca@befair.it>
2017-01-24 08:05:19 +0100

    aggiornamenti vari

--- fibo.py
+++ fibo.py
@@ -10,21 +10,57 @@ fib(3) --> 2
 fib(n) --> fib(n-1) + fib(n-2)
 """
 import sys
+import types
+import collections
+
+d = {
+    "inizializza": "fibonacci",
+    0: 0,
+    1: 1,
+}
+
+d = collections.OrderedDict([
+    ("inizializza", "fibonacci"), (0,0), (1,1)
+])
+
 
 def calc_fibo(n):
-    if n == 0: 
-        return 0
-    elif n == 1:
-        return 1
+    """
+    Calcola la funzione di Fibonacci, appoggiandosi ad una cache
 
-    return calc_fibo(n - 1) + calc_fibo(n-2)
+    :param n: un intero positivo di input
+    :return: il valore (intero) di Fibonacci per n
+    """
 
-if __name__ == "__main__":
-    
-    if len(sys.argv) < 2:
-        print("Usage {} <num>".format(sys.argv[0]))
+    if n in d:
+        rv = d[n]
+    elif n < 0:
+        raise ValueError("Fibo not supported for negative numbers ({})".format(n))
+    # elif type(n) != types.IntType:
+    elif not isinstance(n, types.IntType):
+        raise TypeError("\CORSO: type {} is not valid".format(type(n)))
+    else:
+        rv = calc_fibo(n - 1) + calc_fibo(n - 2)
+        d[n] = rv
+        d["stringa{}".format(n)] = "pippo"
+
+    return rv
+
+def main(argv):
+
+    if len(argv) < 2:
+        print("Usage {} <num>".format(argv[0]))
         sys.exit(1)
 
-    n = int(sys.argv[1])
+    n = int(argv[1])
     rv = calc_fibo(n)
-    print("fibo({0}) = {1}".format(n, rv))
+    print("LA SEQUENZA DI FIBONACCI PER {}: ".format(n))
+
+    for k, v in d.iteritems():
+        print("fibo({}) = {}".format(k, v))
+
+    for k, v in sorted(d.items()):
+        print("fibo({}) = {}".format(k, v))
+
+if __name__ == "__main__":
+    main(sys.argv)    
--- test_fibo.py
+++ test_fibo.py
@@ -3,6 +3,12 @@ import unittest
 from fibo import calc_fibo
 
 class TestMyFibo(unittest.TestCase):
+    
+    # def setUp(self):
+    #    print("avvio test")
+
+    # def tearDown(self):
+    #    print("fine test")
 
     def test_base(self):
         self.assertEqual(calc_fibo(0), 0)
@@ -21,9 +27,12 @@ class TestMyFibo(unittest.TestCase):
             calc_fibo(-1)
     
     def test_string(self):
-        with self.assertRaises(TypeError):
+        with self.assertRaises(TypeError) as cm:
             calc_fibo("ciao")
             calc_fibo("12")
+
+        the_exception = cm.exception
+        self.assertRegexpMatches(str(the_exception), r"\\CORSO:")
 
 if __name__ == '__main__':
     unittest.main()
